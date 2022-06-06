91932f380ea0e62122df08bd4ba55f7e4f508897
NamNe123 <90250775+NamNe123@users.noreply.github.com>
2022-06-06 23:43:00 +0700

    Initial commit

---
+++ Lab3_/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lab3_.Startup))]
+namespace Lab3_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
