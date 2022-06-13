bd6e17aa2b15f9b26997ba260e90bde1bd76c220
NamNe123 <90250775+NamNe123@users.noreply.github.com>
2022-06-13 12:43:12 +0700

    New

--- Lab3_/Models/ApplicationDbContext.cs
+++ Lab3_/Models/ApplicationDbContext.cs
@@ -9,8 +9,10 @@ namespace Lab3_.Models
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public DbSet<Course> Course { get; set; }
+        public DbSet<Course> Courses { get; set; }
         public DbSet<Category> Categories { get; set; }
+   
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
